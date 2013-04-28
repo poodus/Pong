7912a386a06e383fe871cd1761a4ef9fddf65ae6
The Iron Marx <theironmarx@Theorem.(none)>
2013-04-28 18:05:00 -0500

    Small fixes to increasing size

--- ePing.cpp
+++ ePing.cpp
@@ -696,7 +696,7 @@ int main(int argc, const char** argv)
 						
 						//Subtract growth from initial once so when we ping, we can add sizeGrowth to it every time,
 						//and initialGrowth is still proper
-						sizeInitial -= sizeGrowth;
+						icmpPayloadLength = sizeInitial - sizeGrowth;
 					}
 					else
 					{
